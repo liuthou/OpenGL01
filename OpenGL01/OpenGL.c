3d9edba07105af7a35fadb875ff5bb3292198f63
Liuthou <liuthou@163.com>
2014-10-16 11:53:13 +0800

    add OpenGL02

--- OpenGL01/OpenGL.c
+++ OpenGL01/OpenGL.c
@@ -152,7 +152,7 @@ void myDisplay(void)
 #pragma endregion
 
 #pragma region 制定着色模型
-/**/
+/*
 const GLdouble Pi = 3.1415926536;
 void myDisplay(void)
 {
@@ -168,6 +168,39 @@ void myDisplay(void)
 	}
 	glEnd();
 	glFlush();
+}*/
+#pragma endregion
+#pragma region 太阳月亮地球
+/**/
+static int day = 55; //day的变化 0~365
+void myDisplay(void)
+{
+	glEnable(GL_DEPTH_TEST);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(75, 1, 1, 400000000);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0, -200000000, 200000000, 0, 0, 0, 0, 0, 1);
+
+	// 绘制红色的“太阳”
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glutSolidSphere(69600000, 20, 20);
+	// 绘制蓝色的“地球”
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glRotatef(day / 360.0*360.0, 0.0f, 0.0f, -1.0f);
+	glTranslatef(150000000, 0.0f, 0.0f);
+	glutSolidSphere(15945000, 20, 20);
+	// 绘制黄色的“月亮”
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glRotatef(day / 30.0*360.0 - day / 360.0*360.0, 0.0f, 0.0f, -1.0f);
+	glTranslatef(38000000, 0.0f, 0.0f);
+	glutSolidSphere(4345000, 20, 20);
+
+	glFlush();
+
 }
 #pragma endregion
 
